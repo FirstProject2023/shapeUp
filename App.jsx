594a1ce7383fe115816ae0dcfa78f7eaea2ec90c
Shneor Shryber <ABHTUR321@gmail.com>
2023-01-17 11:19:08 +0200

    testNum2

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import { Button, StyleSheet, Text, View,} from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-    <Text>dnkjvnskfd</Text>
+    <Text>dnkjvnskfd!!!!!!!!!!!!!!!</Text>
 
       <StatusBar style="auto" />
     </View>
