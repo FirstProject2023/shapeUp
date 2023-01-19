2745c738322e230ac143ed800b8cf4c549d6b29d
Shneor Shryber <ABHTUR321@gmail.com>
2023-01-19 11:18:40 +0200

    56

--- Components/Home/Home.jsx
+++ Components/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { View,Text } from 'react-native'
 import { MyCarousel } from '../MyCarousel'
+import TestJ from '../../tests/TestJ'
 
 
 export default function Home() {
   return (
    <View>
+   
     <Text>Home</Text>
+    <TestJ/>
    </View>
   )
 }
---
+++ tests/TestJ.jsx
@@ -0,0 +1,14 @@
+import React,{ useState ,useEffect} from 'react';
+import { View,Text } from 'react-native'
+
+import data from "../Jsons/articles.json"
+
+export default function TestJ() {
+
+  return (
+   <View>
+    <Text>
+    </Text>
+   </View>
+  )
+}
