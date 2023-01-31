e7f41d4cfcdca1a2e9e5816b73d7ba089e062ac1
Maoz Cohen <maoz0542588518@gmail.com>
2023-01-31 11:17:43 +0200

    mama

--- App.jsx
+++ App.jsx
@@ -21,6 +21,7 @@ export default function App() {
    
 <NavigationContainer>
   <Stack.Navigator>
+    <Stack.Screen name='Login' component={Login}/>
     <Stack.Screen name="Nav" component={Nav} options={{headerShown: false}}/>
     <Stack.Screen name='Home' component={Home}/>
     <Stack.Screen name='Article' component={ArticleCard}/>
