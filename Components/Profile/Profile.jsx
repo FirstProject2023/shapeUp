bbe12b2da7d1e7325140aeebf9e4793b8ecda77f
Shneor Shryber <ABHTUR321@gmail.com>
2023-01-31 15:37:17 +0200

    sfsd

--- Components/Profile/Profile.jsx
+++ Components/Profile/Profile.jsx
@@ -1,12 +1,81 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,TouchableOpacity,ImageBackground  } from 'react-native'
 import React from 'react'
+import { auth } from '../../firebase';
 
-export default function Profile() {
+export default function Profile({ navigation }) {
+
+
+
+  const hendleSingOut =()=>{
+    auth
+    .signOut()
+    .then(()=>{
+      navigation.navigate('Login')
+    })
+    .catch(error => log("error"))
+  }
+
+
+if(auth.currentUser)
+{
   return (
-    <View>
-      <Text>Profile</Text>
+    
+    <ImageBackground source={{uri: "https://images.indianexpress.com/2021/12/GettyImages-fasting-diet-plan-1200.jpg"}} resizeMode= 'cover'>
+    <View style={styles.container}>
+
+
+      <Text style={{fontSize:30,color:'white'}}> Your Profile</Text>
+      <Text style={{fontSize:20,color:'white'}}>your email: {auth.currentUser?.email}</Text>
+
+      <TouchableOpacity
+    style={styles.loginButton}
+    onPress={hendleSingOut}
+    >
+        <Text style={{color: '#fff', fontSize: 20}}>Sign Out</Text>
+    </TouchableOpacity>  
+     
+
     </View>
+    </ImageBackground>
   )
 }
+else{
+  return (
+    <ImageBackground source={{uri: "https://images.indianexpress.com/2021/12/GettyImages-fasting-diet-plan-1200.jpg"}} resizeMode= 'cover'>
+    <View style={styles.container}>
 
-const styles = StyleSheet.create({})
+
+    <Text style={{fontSize:30,color:'white'}}>Your Profile </Text>
+    <TouchableOpacity
+    style={styles.loginButton}
+    onPress={hendleSingOut}
+    >
+        <Text style={{color: '#fff', fontSize: 20}}>Creat auser</Text>
+    </TouchableOpacity>  
+
+    </View>
+    </ImageBackground>
+  )
+}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'rgba(0,0,0,0.6)',
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  loginButton:{
+    width: '60%',
+    height: '10%',
+    backgroundColor: '#0782f9',
+    marginTop: 15,
+    borderWidth: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+
+},
+})
