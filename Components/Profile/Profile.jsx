00614d8c5baea515847a83d32f434d80e29dcc15
Shneor Shryber <ABHTUR321@gmail.com>
2023-02-02 18:40:18 +0200

    n

--- Components/Profile/Profile.jsx
+++ Components/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View,TouchableOpacity,ImageBackground,Image, ScrollView  } from 'react-native'
+import { StyleSheet, Text, View,TouchableOpacity,ImageBackground,Image, ScrollView,TextInput  } from 'react-native'
 import React, { useState } from 'react'
 import { auth } from '../../firebase';
 import { oreng } from '../Globals/colors';
@@ -8,10 +8,17 @@ import { MaterialIcons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons'; 
 import { Foundation } from '@expo/vector-icons';
 import { EvilIcons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+
 
 export default function Profile({ navigation }) {
 
 const [password,setPassword]=useState('12378asd3');
+const [nameInput,setNameInput] = useState(1);
+const [ageInput,setageInput] = useState(1);
+const [dateInput,setInput] = useState(1);
+const [passwordInput,setpasswordInput] = useState(1);
 
   const hendleSingOut =()=>{
     auth
@@ -22,22 +29,31 @@ const [password,setPassword]=useState('12378asd3');
     .catch(error => log("error"))
   }
 
+  function ChangeNameInpout()
+  {
+    setNameInput(!nameInput)
+  }
+  function ChangeAgeInpout()
+  {
+    setAgeInput(!nameInput)
+  }
+  function ChangeDateInput()
+  {
+    setDateInput(!nameInput)
+  }
+  function ChangePasswordInput()
+  {
+    setPasswordInput(!nameInput)
+  }
 
 if(auth.currentUser)
 {
   return (
     
-
-    
-    
-    
     <View style={styles.container}>
-
 
       <View style={styles.profileImg}>
         
-
-
         <ImageBackground
         style={{height:'100%',width:'100%'}}
         source={{uri: "https://images.indianexpress.com/2021/12/GettyImages-fasting-diet-plan-1200.jpg"} }
@@ -48,25 +64,28 @@ if(auth.currentUser)
           </View>
         </ImageBackground>
 
-
-
       </View>
 
       
-      <TouchableOpacity style={{zIndex:90}}>
+    <View style={{zIndex:90}}>
         <View style={styles.circle}>
         <Image style={{height:'100%',width:'100%',borderRadius: 50}} source={{uri: "https://www.seekpng.com/png/detail/966-9665493_my-profile-icon-blank-profile-image-circle.png"}} resizeMode= 'cover'/>
-        <EvilIcons name="plus" size={54} color="black" style={{ position:'absolute',top:68,right:0}}/>
         </View>
-      </TouchableOpacity>
+    </View>
+  
+
+      <TouchableOpacity style={{zIndex:100}}>
+        <View style={{ width:28,height:30,backgroundColor:'white',borderRadius:50,position:'absolute',top:12,left:113}} />
+       <Feather name="plus-circle" size={36} color="black" style={{ position:'absolute',top:10,left:110}} /> 
+        </TouchableOpacity>
 {/* a */}
 
 
 
       <ScrollView>
 
-<View style={{height:'12%',width:'100%',justifyContent:'center',alignItems:'center' ,backgroundColor:'red'}}>
-      <Text style={{fontSize:20,color:'white',marginRight:110,fontSize:20}}> shneor shrybr</Text>
+<View style={{height:'12%',width:'100%',justifyContent:'center',alignItems:'center' }}>
+      <Text style={{fontSize:20,color:'white',marginRight:130,fontSize:25}}> shneor shrybr</Text>
       <Text style={{fontSize:20,color:'white',marginRight:30,fontSize:20}}> 21 </Text>
 </View>
 
@@ -97,16 +116,37 @@ if(auth.currentUser)
       </View>
 
 
-
-
-
 <View style={{width:'79%',justifyContent:'center',alignItems:'center'}}>
 
 
+            {
+                  nameInput 
+                  ?
           <View style={styles.details}>
+          
+              <FontAwesome5 name="pen" size={20} color="black" style={{marginLeft:15}}
+              onPress={ChangeNameInpout}
+              />
           <Text style={{fontSize:15,color:"black",textAlign:'center',marginStart:50}}> shneor shryber </Text>
           <Ionicons style={{marginEnd:20}} name="md-man" size={24} color="black" />
+
           </View>
+
+          : 
+
+          <View style={{width:'100%',alignItems:'center',flexDirection:'row'}}>
+             <FontAwesome5 name="pen" size={20} color="black" style={{marginLeft:15}}
+              onPress={ChangeNameInpout}
+              />
+              <TextInput placeholder=' Enter here ...' 
+                style={{backgroundColor:'#fff',borderColor:'black',borderWidth:1,width:'80%',marginLeft:15,marginTop:10,height:40}}
+                ></TextInput>
+                
+        </View>
+
+
+
+            }
 <View
     style={{
       height: 1.5,
@@ -116,12 +156,45 @@ if(auth.currentUser)
       marginLeft:28
     }}
   />
+  
 
 
-<View style={styles.details}>
-<Text style={{fontSize:15,color:"black",textAlign:'center',marginStart:50}}> 21 </Text>
+
+{/* <View style={styles.details}>
+<Text style={{fontSize:15,color:"black",marginStart:50}}>21 </Text>
 <MaterialIcons  style={{marginEnd:20}} name="date-range" size={24} color="black" />
-</View>
+</View> */}
+
+{
+  
+                  nameInput 
+                  ?
+          <View style={styles.details}>
+          
+              <FontAwesome5 name="pen" size={20} color="black" style={{marginLeft:15}}
+              onPress={ChangeNameInpout}
+              />
+          <Text style={{fontSize:15,color:"black",textAlign:'center',marginStart:50}}>21</Text>
+          <Ionicons style={{marginEnd:20}} name="md-man" size={24} color="black" />
+
+          </View>
+
+          : 
+
+          <View style={{width:'100%',alignItems:'center',flexDirection:'row'}}>
+             <FontAwesome5 name="pen" size={20} color="black" style={{marginLeft:15}}
+              onPress={ChangeNameInpout}
+              />
+              <TextInput placeholder=' Enter here ...' 
+                style={{backgroundColor:'#fff',borderColor:'black',borderWidth:1,width:'80%',marginLeft:15,marginTop:10,height:40}}
+                ></TextInput>
+                
+        </View>
+
+
+
+            }
+
 <View
     style={{
       height: 1.5,
@@ -137,6 +210,7 @@ if(auth.currentUser)
 <Text style={{fontSize:15,color:"black",textAlign:'center',marginStart:50}}> {auth.currentUser?.email} </Text>
 <MaterialIcons style={{marginEnd:20}} name="email" size={24} color="black" />
 </View>
+
 <View
     style={{
       height: 1.5,
@@ -188,7 +262,7 @@ if(auth.currentUser)
     }}
   />
 
-    <View style={{width:'100%', alignItems: 'center',}}>
+    <View style={{width:'100%', height:85,alignItems:'center' }}>
       <TouchableOpacity
     style={styles.loginButton}
     onPress={hendleSingOut}
@@ -257,15 +331,13 @@ profileImg:
   height: '25%',
 },
 circle: {
-  width: 100,
-  height: 100,
+  width: 80,
+  height: 80,
   borderWidth:2,
   borderRadius: 50,
  position:'absolute',
- top:-55,
-left:50,
-
-  
+ top:-46,
+left:70,
 },
 weightContainer:{
   marginTop:10,
@@ -285,8 +357,6 @@ details:{
   alignItems:'center',
   justifyContent:'center',
   justifyContent:'space-between'
-
-
 
 },
 
